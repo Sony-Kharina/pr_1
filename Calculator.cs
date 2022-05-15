15470f1eb4f819f8112fd4e88dafc6c10980ea09
Sony-Kharina <105545053+Sony-Kharina@users.noreply.github.com>
2022-05-15 17:31:14 +0300

    Update and rename Program.cs to Calculator.cs

---
+++ Calculator.cs
@@ -0,0 +1,52 @@
+using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = 0; int num2 = 0; string v = "0";
+            Console.WriteLine("Здраствуйте\r");
+            while (v != "c")
+            {
+                Console.WriteLine("Что Вы хотите сделать?:");
+                Console.WriteLine("\ta - Сложение");
+                Console.WriteLine("\tb - Умножение");
+                Console.WriteLine("\tc - Выход");
+                Console.Write("Выбор: ");
+                v = Console.ReadLine();
+                while (v != "a" & v != "b" & v != "c")
+                {
+                    Console.WriteLine("Недопустимое значение, попробуйте снова: ");
+                    v = Console.ReadLine();
+                }
+                if (v != "c")
+                {
+                    Console.WriteLine("Введите первое число: ");
+                    num1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Введите второе число: ");
+                    num2 = Convert.ToInt32(Console.ReadLine());
+                }
+
+                // Use a switch statement to do the math.
+                switch (v)
+                {
+                    case "a":
+                        Console.WriteLine($"Результат: {num1} + {num2} = " + (num1 + num2));
+                        break;
+
+                    case "b":
+                        Console.WriteLine($"Результат: {num1} * {num2} = " + (num1 * num2));
+                        break;
+                    case "c":
+                        Console.WriteLine("До свидания");
+                        break;
+
+                }
+            }
+        }
+    }
+}
