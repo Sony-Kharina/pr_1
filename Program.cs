4cb6911484654ae7c9a4d6ed0d83bd38fcd75b34
Sony-Kharina <105545053+Sony-Kharina@users.noreply.github.com>
2022-05-15 15:28:44 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SimpleCSharpApp
+{
+class Program
+{
+static void Main(string [ ] args){
+Console .WnteLine ("***** My First C# Арр *****");
+Console . WnteLine (’’Hello World' ”);
+Console. WnteLine ();
+Console.ReadLine ();
+}
+}
+}
