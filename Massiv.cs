f84d82371eb3fe99cd6ffe8e341e29087f229470
Sony-Kharina <105545053+Sony-Kharina@users.noreply.github.com>
2022-05-30 21:57:02 +0300

    Update Massiv.cs
    Создан транспонированный массив, исправлены другие замечания

--- Massiv.cs
+++ Massiv.cs
@@ -6,50 +6,52 @@ namespace Massiv_
     {
         static void Main(string[] args)
         {
-            Console.Write("Сколько столбцов: ");
-            int x = int.Parse(Console.ReadLine());
             Console.Write("Сколько строк: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Сколько столбцов: ");
             int y = int.Parse(Console.ReadLine());
             int[,] mas = new int[x, y];
+            int[,] mas2 = new int[y, x];
             Console.WriteLine();
             string s = "0";
             Console.WriteLine("Заполните двумерный массив, вводя элементы строк через пробел: ");
-            for (int i = 0; i < y; i++)
+            for (int i = 0; i < x; i++)
             {
                 Console.WriteLine($"Строка {i}: ");
-                
-                s = Console.ReadLine();
-                string[] values = s.Split(' ');
+                string[] values = Console.ReadLine().Split();
+  
               
-                    for (int j = 0; j < x; j++)
+                    for (int j = 0; j < y; j++)
                     {
-                        int number;
-
-                        bool success = int.TryParse(values[j], out number);
+                      
+                        bool success = int.TryParse(values[j], out var number);
                         if (success)
                         {
-                            mas[j, i] = number;
+                            mas[i, j] = number;
+                            mas2[j, i] = number;
                         }
                     }
                 }
                 
-                
+            
+
+            
             Console.WriteLine("Вы ввели массив: ");
-            for (int i = 0; i < y; i++)
-            {
-                for (int j = 0; j < x; j++)
-                {
-                    Console.Write(mas[j, i]);
-                }
-                Console.WriteLine();
-            }
-           
-            Console.WriteLine("Транспонированный массив: ");
             for (int i = 0; i < x; i++)
             {
                 for (int j = 0; j < y; j++)
                 {
                     Console.Write(mas[i, j]);
+                }
+                Console.WriteLine();
+            }
+           
+            Console.WriteLine("Транспонированный массив: ");
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    Console.Write(mas2[i, j]);
                 }
                 Console.WriteLine();
             }
