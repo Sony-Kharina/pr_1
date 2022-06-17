40f1d5df654d4b6e1f154717647db2f267131fcc
Sony-Kharina <105545053+Sony-Kharina@users.noreply.github.com>
2022-06-17 15:05:22 +0300

    Update ShowFile.cs

--- ShowFile.cs
+++ ShowFile.cs
@@ -5,119 +5,99 @@ using System.Collections.Generic;
 
 namespace ShowFile
 {
-	class Name : IComparer<Name>
+	class People
 	{
 		public string FIO { set; get; }
-		public int Vozrast { set; get; }
-		public int Oklad { set; get; }
-
-
-		public int Compare(Name item1, Name item2)
+		public string Vozrast { set; get; }
+		public string Salary { set; get; }
+		public People(string fio, string vozrast, string salary)
+		{
+			FIO = fio;
+			Vozrast = vozrast;
+			Salary = salary;
+		}
+		
+	}
+	class NameComparer : IComparer<People>
+	{
+		
+		public int Compare(People item1, People item2)
 		{
 			return string.Compare(item1.FIO, item2.FIO);
 		}
 	}
-	class Age : IComparer<Age>
-	{
-		public string FIO1 { set; get; }
-		public string Vozrast1 { set; get; }
-		public string Oklad1 { set; get; }
-
-
-		public int Compare(Age item1, Age item2)
+		class AgeComparer : IComparer<People>
 		{
-			return string.Compare(item1.Vozrast1, item2.Vozrast1);
-		}
-	}
-	class Money : IComparer<Money>
-	{
-		public string FIO2 { set; get; }
-		public string Vozrast2 { set; get; }
-		public string Oklad2 { set; get; }
 
-	
-		public int Compare(Money item1, Money item2)
-		{
-			return string.Compare(item1.Oklad2, item2.Oklad2);
+			public int Compare(People item1, People item2)
+			{
+				return string.Compare(item1.Vozrast, item2.Vozrast);
+			}
 		}
-	}
+		class SalaryComparer : IComparer<People>
+		{
+
+			public int Compare(People item1, People item2)
+			{
+				return string.Compare(item1.Salary, item2.Salary);
+			}
+		}
+		
 
 	class ShowFile
 	{
 		static void Main(string[] args)
 		{
-			List<Name> People = new List<Name>();
-			List<Age> People1 = new List<Age>();
-			List<Money> People2 = new List<Money>();
-			
-
+			List<People> People1 = new List<People>();
+		
 			try
 			{
 				string[] lines = File.ReadAllLines(args[0]);
-				if (args[1].Equals("ФИО"))
-				{
-					foreach (string s in lines)
-				{
-					string[] q = s.Split(';');
-					bool success = int.TryParse(q[1], out var age);
-					bool success1 = int.TryParse(q[2], out var oklad);
-					
-					People.Add(new Name() { FIO = q[0], Vozrast = age, Oklad = oklad });
-					
-				}
 				
-				
-					People.Sort(new Name());
-					for (int i = 0; i < People.Count; i++)
-					{
-						Name item = People[i];
-						Console.WriteLine("{0} {1} {2}", item.FIO, item.Vozrast, item.Oklad);
-					}
-				}
-				if (args[1].Equals("Возраст"))
-				{
 					foreach (string s in lines)
 					{
 						string[] q = s.Split(';');
-				
-						People1.Add(new Age() { FIO1 = q[0], Vozrast1 = q[1], Oklad1 = q[2] });
+						People1.Add(new People(q[0], q[1], q[2]));
 
 					}
+					if (args[1].Equals("ФИО"))
+					{
 
-
-					People1.Sort(new Age());
+						People1.Sort(new NameComparer());
 					for (int i = 0; i < People1.Count; i++)
-					{
-						Age item = People1[i];
-						Console.WriteLine("{0} {1} {2}", item.FIO1, item.Vozrast1, item.Oklad1);
+						{
+						People item = People1[i];
+						Console.WriteLine("{0} {1} {2}", item.FIO, item.Vozrast, item.Salary);
+						}
 					}
-				}
-
-				if (args[1].Equals("Оклад"))
-				{
-					foreach (string s in lines)
+				
+					if (args[1].Equals("Возраст"))
 					{
-						string[] q = s.Split(';');
-					
-						People2.Add(new Money() { FIO2 = q[0], Vozrast2 = q[1], Oklad2 = q[2] });
-
+						People1.Sort(new AgeComparer());
+					for (int i = 0; i < People1.Count; i++)
+						{
+						People item = People1[i];
+						Console.WriteLine("{0} {1} {2}", item.FIO, item.Vozrast, item.Salary);
+						}
 					}
-
-
-					People2.Sort(new Money());
-					for (int i = 0; i < People2.Count; i++)
+		
+					if (args[1].Equals("Оклад"))
 					{
-						Money item = People2[i];
-						Console.WriteLine("{0} {1} {2}", item.FIO2, item.Vozrast2, item.Oklad2);
+						People1.Sort(new SalaryComparer());
+					for (int i = 0; i < People1.Count; i++)
+						{
+						People item = People1[i];
+						Console.WriteLine("{0} {1} {2}", item.FIO, item.Vozrast, item.Salary);
+						}
 					}
-				}
 			}
 			catch (IOException exc)
 			{
 				Console.WriteLine("Ошибка чтения файла");
 				Console.WriteLine(exc.Message);
 			}
-			
+
 		}
 	}
 }
+
